cfeb987fc612a2ce733ca58d147619b223362ca3
Those45Ninjas <tom.parker1235@gmail.com>
2019-06-10 11:40:54 +0800

    Removed un-used tintColour from generic shader

--- kau-game/resources/shaders/generic.frag.glsl
+++ kau-game/resources/shaders/generic.frag.glsl
@@ -1,11 +1,10 @@
 #version 330
 
-uniform vec4 tintColor;
 uniform sampler2D PrimaryTexture;
 
 in vec2 TexCoord;
 out vec4 FragColor;
 
 void main() {
-    FragColor = texture(PrimaryTexture, TexCoord) * tintColor;
+    FragColor = texture(PrimaryTexture, TexCoord);
 }
