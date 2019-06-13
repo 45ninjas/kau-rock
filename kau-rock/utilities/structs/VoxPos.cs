4bfa6e0d269e92ec6946c7221323214671b0cd8c
Those45Ninjas <tom.parker1235@gmail.com>
2019-06-14 01:15:58 +0800

    Added operator support to VoxPos

--- kau-rock/utilities/structs/VoxPos.cs
+++ kau-rock/utilities/structs/VoxPos.cs
@@ -6,8 +6,32 @@ namespace KauRock {
 		public int Y;
 		public int Z;
 
-		static public implicit operator Vector3(VoxPos pos) {
+		public VoxPos(int X, int Y, int Z) {
+			this.X = X;
+			this.Y = Y;
+			this.Z = Z;
+		}
+
+		public static implicit operator Vector3(VoxPos pos) {
 			return new Vector3(pos.X,pos.Y,pos.Z);
+		}
+		public static VoxPos operator + (VoxPos a, VoxPos b) {
+			return new VoxPos(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+		}
+		public static VoxPos operator - (VoxPos a, VoxPos b) {
+			return new VoxPos(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+		}
+		public static VoxPos operator * (VoxPos a, VoxPos b) {
+			return new VoxPos(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+		}
+		public static VoxPos operator * (VoxPos a, int b) {
+			return new VoxPos(a.X * b, a.Y * b, a.Z * b);
+		}
+		public static VoxPos operator / (VoxPos a, VoxPos b) {
+			return new VoxPos(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+		}
+		public static VoxPos operator / (VoxPos a, int b) {
+			return new VoxPos(a.X / b, a.Y / b, a.Z / b);
 		}
 	}
 }
