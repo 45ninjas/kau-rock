78b95bd5b221b623e81dce45f8b0d2699f5a44e3
Tom Parker <tom@tomp.id.au>
2020-08-29 09:51:12 +0800

    Messded about with mesh maker

--- kau-rock/terrain/MeshMaker.cs
+++ kau-rock/terrain/MeshMaker.cs
@@ -76,8 +76,42 @@ namespace KauRock.Terrain {
       // Set the new mesh on the chunk.
       // chunk.SetMesh(vertices.ToArray(), triangles.ToArray());
     }
-    void March (VoxPos position) {
+    void March (VoxPos pos) {
+      // Left face.
+      if ( !isSolid( pos + VoxPos.Left ) ) {
+        triangle(pos, 1, 5, 4);
+        triangle(pos, 1, 4, 0);
+      }
 
+      // Right face.
+      if ( !isSolid( pos + VoxPos.Right ) ) {
+        // triangle(pos, 3, 7, 6);
+        // triangle(pos, 3, 6, 2);
+      }
+
+      // Top face.
+      if ( !isSolid( pos + VoxPos.Up ) ) {
+        // triangle(pos, 6, 7, 4);
+        // triangle(pos, 6, 4, 5);
+      }
+
+      // Bottom face.
+      if ( !isSolid( pos + VoxPos.Down ) ) {
+        // triangle(pos, 3, 2, 1);
+        // triangle(pos, 3, 1, 0);
+      }
+
+      // Front face.
+      if ( !isSolid( pos + VoxPos.Forward ) ) {
+        // triangle(pos, 4, 7, 3);
+        // triangle(pos, 4, 3, 0);
+      }
+
+      // Back face.
+      if ( !isSolid( pos + VoxPos.Backwards ) ) {
+        // triangle(pos, 6, 5, 1);
+        // triangle(pos, 6, 1, 2);
+      }
     }
     public void doCube (VoxPos pos, Chunk chunk) {
 
@@ -171,21 +205,21 @@ namespace KauRock.Terrain {
     // 		triangle(pos, 4, 3, 0);
     // 	}
     // }
-    // private void triangle(VoxPos position, int a, int b, int c) {
-    // 	triangles.Push((uint)getVertex(position + corner[a]));
-    // 	triangles.Push((uint)getVertex(position + corner[b]));
-    // 	triangles.Push((uint)getVertex(position + corner[c]));
-    // }
-    // private int getVertex(VoxPos position) {
-    // 	int index = position.X + position.Y * LookupSize + position.Z * LookupSize * LookupSize;
+    private void triangle(VoxPos position, int a, int b, int c) {
+    	triangles.Push(getVertex(position + corner[a]));
+    	triangles.Push(getVertex(position + corner[b]));
+    	triangles.Push(getVertex(position + corner[c]));
+    }
+    private int getVertex(VoxPos position) {
+    	int index = position.X + position.Y * LookupSize + position.Z * LookupSize * LookupSize;
 
-    // 	// If the vert does not exist, add it.
-    // 	 if(vertLookup[index] < 0) {
-    // 		vertLookup[index] = verticesCount++;
-    // 		vertices.Push(new Chunk.Vertex(position));
-    // 	}
+    	// If the vert does not exist, add it.
+    	 if(vertLookup[index] < 0) {
+    		vertLookup[index] = verticesCount++;
+    		vertices.Push(new Chunk.Vertex(position));
+    	}
 
-    // 	return vertLookup[index];
-    // }
+    	return vertLookup[index];
+    }
   }
 }
